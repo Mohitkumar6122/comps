b8faf9d6e21a446d290234be603bcf8f6dae9cec
Mohit Kumar <mkay6123@gmail.com>
2023-01-07 06:29:33 +0530

    Moved dropdown logic to dropdownPage
    Signed-off-by: Mohit Kumar <mkay6123@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,43 +1,8 @@
-import React, { useState } from "react";
-import Dropdown from "./components/Dropdown";
+import React from "react";
+import DropdownPage from "./pages/DropdownPage";
 function App() {
-  const [selection, setSelection] = useState(null);
-  const handleSelection = (option) => {
-    setSelection(option);
-  }
-  console.log(selection)
-  const options = [
-    {
-      label: "The Color Red",
-      value: "red",
-    },
-    {
-      label: "The Color Green",
-      value: "green",
-    },
-    {
-      label: "A Shade of Blue",
-      value: "blue",
-    },
-    {
-      label: "A Shade of Pink",
-      value: "pink",
-    },
-  ];
-
-  return (
-    <div className="flex">
-      <Dropdown
-        options={options}
-        value={selection}
-        onChange={handleSelection}
-      />
-      <Dropdown
-        options={options}
-        value={selection}
-        onChange={handleSelection}
-      />
-    </div>
+    return (
+    <div><DropdownPage /></div>
   );
 }
 
---
+++ src/pages/DropdownPage.js
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+import Dropdown from "../components/Dropdown";
+function DropdownPage() {
+  const [selection, setSelection] = useState(null);
+  const handleSelection = (option) => {
+    setSelection(option);
+  };
+  const options = [
+    {
+      label: "The Color Red",
+      value: "red",
+    },
+    {
+      label: "The Color Green",
+      value: "green",
+    },
+    {
+      label: "A Shade of Blue",
+      value: "blue",
+    },
+    {
+      label: "A Shade of Pink",
+      value: "pink",
+    },
+  ];
+
+  return (
+    <div className="flex">
+      <Dropdown
+        options={options}
+        value={selection}
+        onChange={handleSelection}
+      />
+      <Dropdown
+        options={options}
+        value={selection}
+        onChange={handleSelection}
+      />
+    </div>
+  );
+}
+
+export default DropdownPage;
