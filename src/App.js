93dc7118603e5e87988fa1c926df2aee0640924a
Mohit Kumar <mkay6123@gmail.com>
2023-01-08 02:35:37 +0530

    Added Routing
    Signed-off-by: Mohit Kumar <mkay6123@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
-import React from "react";
 import DropdownPage from "./pages/DropdownPage";
+import AccordionPage from "./pages/AccordionPage";
+import Route from "./components/Route";
+import Link from "./components/Link";
 function App() {
-    return (
-    <div><DropdownPage /></div>
+  return (
+    <div>
+      <div>
+        <Link to="/accordion"> Go to accordion</Link>
+        <Link to="/dropdown"> Go to dropdown</Link>
+      </div>
+      <div>
+        <Route path="/accordion">
+          <AccordionPage />
+        </Route>
+        <Route path="/dropdown">
+          <DropdownPage />
+        </Route>
+      </div>
+    </div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
-import './index.css'
+import "./index.css";
+import { NavigationProvider } from "./context/navigation";
 
-createRoot(document.querySelector("#root")).render(<App />);
- 
+createRoot(document.querySelector("#root")).render(
+  <NavigationProvider>
+    <App />
+  </NavigationProvider>
+);
