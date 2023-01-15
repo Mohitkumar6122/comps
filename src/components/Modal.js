1ac8f1ea593039b49cbadd36257aabad729a6534
Mohit Kumar <mkay6123@gmail.com>
2023-01-15 18:50:01 +0530

    Added Modal with React-portal
    Signed-off-by: Mohit Kumar <mkay6123@gmail.com>

---
+++ src/components/Modal.js
@@ -0,0 +1,31 @@
+import React, { useEffect } from "react";
+import ReactDOM from "react-dom";
+function Modal({ onClose, children, actionBar }) {
+  
+  // used to disable scrolling when modal is open
+  useEffect(() => {
+    document.body.classList.add("overflow-hidden");
+    //cleanup function for the useeffect
+    return () => {
+      document.body.classList.remove("overflow-hidden");
+    };
+  }, []);
+
+  return ReactDOM.createPortal(
+    <div>
+      <div
+        onClick={onClose}
+        className="fixed inset-0 bg-gray-300 opacity-80"
+      ></div>
+      <div className="fixed inset-40 p-10 bg-white">
+        <div className="flex flex-col justify-between h-full">
+          {children}
+          <div className="flex justify-end">{actionBar}</div>
+        </div>
+      </div>
+    </div>,
+    document.querySelector(".modal-container")
+  );
+}
+
+export default Modal;
---
+++ src/pages/ModalPage.js
@@ -0,0 +1,36 @@
+import React, { useState, useEffect } from "react";
+import Modal from "../components/Modal";
+import Button from "../components/Button";
+function ModalPage() {
+  const [showModal, setShowModal] = useState(false);
+
+  const handleShowModal = () => {
+    setShowModal(true);
+  };
+  const handleCloseModal = () => {
+    setShowModal(false);
+  };
+
+  const actionBar = (
+    <Button primary success onClick={handleCloseModal}>
+      I Accept
+    </Button>
+  );
+
+  const modal = (
+    <Modal onClose={handleCloseModal} actionBar={actionBar}>
+      <p>Here is an important agreement for you to accept</p>
+    </Modal>
+  );
+
+  return (
+    <div>
+      <Button primary onClick={handleShowModal}>
+        Open Modal
+      </Button>
+      {showModal && modal}
+    </div>
+  );
+}
+
+export default ModalPage;
